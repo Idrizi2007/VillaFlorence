7a18bf29d33afdfed92440b0ef7ce24dab06fa6c
Bjorn Idrizi <idrizibjorn@gmail.com>
2026-04-06 18:55:32 +0200

    Redesign homepage, add Guest Access page & contact options
    Major redesign of Index.cshtml for a modern look: new hero, simplified sections, improved floor navigation, and added map. Added Guest Access page with carousel and contact info. Updated _Layout.cshtml with transparent navbar, floating call button, and improved navigation. These changes enhance usability, highlight amenities, and make guest contact easier.

---
+++ VillaFlorenceAlbania/Pages/GuestAccess.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VillaFlorenceAlbania.Pages
+{
+    public class GuestAccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
