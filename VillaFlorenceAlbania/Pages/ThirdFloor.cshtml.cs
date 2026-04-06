03791c7174983c9a589f1a007e02355d3bfddf15
Bjorn Idrizi <idrizibjorn@gmail.com>
2026-04-06 16:28:28 +0200

    Add project files.

---
+++ VillaFlorenceAlbania/Pages/ThirdFloor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VillaFlorenceAlbania.Pages
+{
+    public class ThirdFloorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
